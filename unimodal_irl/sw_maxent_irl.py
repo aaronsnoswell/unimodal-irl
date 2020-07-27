53cd20d1ab81b7156a202c44fadd6e074c3d712a
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-27 17:42:06 +1000

    Add log-space functions for forwrad and backward message passing variables

--- unimodal_irl/sw_maxent_irl.py
+++ unimodal_irl/sw_maxent_irl.py
@@ -3,6 +3,10 @@ import copy
 import numpy as np
 
 
+# Placeholder for 'negative infinity' which doesn't cause NaN in log-space operations
+_NINF = np.finfo(np.float64).min
+
+
 def backward_pass(p0s, L, t_mat, parents, gamma=1.0, rs=None, rsa=None, rsas=None):
     """Compute backward message passing variable
     
@@ -36,12 +40,72 @@ def backward_pass(p0s, L, t_mat, parents, gamma=1.0, rs=None, rsa=None, rsas=Non
                     alpha[s1, t] *
                     t_mat[s1, a, s2] *
                     np.exp(
-                        gamma ** ((t + 1)-1) * (rsa[s1, a] + rsas[s1, a, s2]) +
+                        gamma ** ((t + 1) - 1) * (rsa[s1, a] + rsas[s1, a, s2]) +
                         (gamma ** (t + 1)) * rs[s2]
                     )
                 )
     return alpha
 
+
+def backward_pass_log(p0s, L, t_mat, parents, gamma=1.0, rs=None, rsa=None, rsas=None):
+    """Compute backward message passing variable in log-space
+    
+    Args:
+        p0s (numpy array): Starting state probabilities
+        L (int): Maximum path length
+        t_mat (numpy array): |S|x|A|x|S| transition matrix
+        parents (dict): Dictionary mapping states to (s, a) parent tuples
+        gamma (float): Discount factor
+        rs (numpy array): Linear state reward weights
+        rsa (numpy array): Linear state-action reward weights
+        rsas (numpy array): Linear state-action-state reward weights
+    
+    Returns:
+        (numpy array): |S| x L array of backward message values in log space
+    """
+    
+    if rs is None:
+        rs = np.zeros(t_mat.shape[0])
+    if rsa is None:
+        rsa = np.zeros(t_mat.shape[0:2])
+    if rsas is None:
+        rsas = np.zeros(t_mat.shape[0:3])
+    
+    alpha = np.zeros((t_mat.shape[0], L))
+    with np.errstate(divide='ignore'):
+        alpha[:, 0] = np.log(p0s) + rs
+        
+    for t in range(L - 1):
+        for s2 in range(t_mat.shape[0]):
+            # Find maximum value among all parents of s2
+            m_t = _NINF
+            for s1, a in parents[s2]:
+                m_t = max(
+                    m_t,
+                    (
+                        alpha[s1, t] +
+                        np.log(t_mat[s1, a, s2]) +
+                        gamma ** ((t + 1) - 1) * (rsa[s1, a] + rsas[s1, a, s2])
+                    )
+                )
+            m_t += (gamma ** (t + 1)) * rs[s2]
+            
+            # Compute next column of alpha in log-space
+            for s1, a in parents[s2]:
+                alpha[s2, t + 1] += (
+                    t_mat[s1, a, s2] *
+                    np.exp(
+                        alpha[s1, t] +
+                        gamma ** ((t + 1) - 1) * (rsa[s1, a] + rsas[s1, a, s2]) +
+                        (gamma ** (t + 1)) * rs[s2] -
+                        m_t
+                    )
+                )
+            with np.errstate(divide='ignore'):
+                alpha[s2, t + 1] = m_t + np.log(alpha[s2, t + 1])
+            
+    return alpha
+    
 
 def forward_pass(L, t_mat, children, gamma=1.0, rs=None, rsa=None, rsas=None):
     """Compute forward message passing variable
@@ -82,6 +146,68 @@ def forward_pass(L, t_mat, children, gamma=1.0, rs=None, rsa=None, rsas=None):
                     ) *
                     beta[s2, t]
                 )
+    return beta
+
+
+def forward_pass_log(L, t_mat, children, gamma=1.0, rs=None, rsa=None, rsas=None):
+    """Compute forward message passing variable in log space
+    
+    Args:
+        L (int): Maximum path length
+        t_mat (numpy array): |S|x|A|x|S| transition matrix
+        children (dict): Dictionary mapping states to (a, s') child tuples
+        gamma (float): Discount factor
+        rs (numpy array): Linear state reward weights
+        rsa (numpy array): Linear state-action reward weights
+        rsas (numpy array): Linear state-action-state reward weights
+    
+    Returns:
+        (numpy array): |S| x L array of forward message values in log space
+    """
+    
+    if rs is None:
+        rs = np.zeros(t_mat.shape[0])
+    if rsa is None:
+        rsa = np.zeros(t_mat.shape[0:2])
+    if rsas is None:
+        rsas = np.zeros(t_mat.shape[0:3])
+    
+    beta = np.zeros((t_mat.shape[0], L))
+    beta[:, 0] = gamma ** (L - 1) * rs
+    for t in range(L - 1):
+        for s1 in range(t_mat.shape[0]):
+            # Find maximum value among children of s1
+            m_t = _NINF
+            for a, s2 in children[s1]:
+                m_t = max(
+                    m_t,
+                    (
+                        np.log(t_mat[s1, a, s2]) +
+                        gamma ** (L - (t + 1) - 1) * (
+                            rsa[s1, a] +
+                            rsas[s1, a, s2]
+                        ) +
+                        beta[s2, t]
+                    )
+                )
+            m_t += gamma ** (L - (t + 1) - 1) * rs[s1]
+            
+            # Compute next column of beta in log-space
+            for a, s2 in children[s1]:
+                beta[s1, t + 1] += (
+                    t_mat[s1, a, s2] *
+                    np.exp(
+                        gamma ** (L - (t + 1) - 1) * (
+                            rs[s1] +
+                            rsa[s1, a] +
+                            rsas[s1, a, s2]
+                        ) +
+                        beta[s2, t] -
+                        m_t
+                    )
+                )
+            beta[s1, t + 1] = m_t + np.log(beta[s1, t + 1])
+            
     return beta
 
 
@@ -209,10 +335,13 @@ def main():
     
     print("Testing Algorithm with Linear MDP example")
     
+    np.random.seed(0)
+    
     env = LinearMDPEnv()
-    env.state_rewards = np.random.randn(4)
-    env.state_action_rewards = np.random.randn(4, 1)
-    env.state_action_state_rewards = np.random.randn(4, 1, 4)
+    reward_scale = 1
+    env.state_rewards = np.random.randn(4) * reward_scale
+    #env.state_action_rewards = np.random.randn(4, 1) * reward_scale
+    #env.state_action_state_rewards = np.random.randn(4, 1, 4) * reward_scale
     env = pad_mdp(env)
     L = 6
     gamma = 0.9
@@ -238,15 +367,15 @@ def main():
     pts_GT[2, 2] = ptau3_GT + ptau4_GT
     pts_GT[3, 3] = ptau4_GT
     
-    ptsa_GT = np.zeros((4, 1, 3))
-    ptsa_GT[0, 0, 0] = ptau2_GT + ptau3_GT + ptau4_GT
-    ptsa_GT[1, 0, 1] = ptau3_GT + ptau4_GT
-    ptsa_GT[2, 0, 2] = ptau4_GT
+    #ptsa_GT = np.zeros((4, 1, 3))
+    #ptsa_GT[0, 0, 0] = ptau2_GT + ptau3_GT + ptau4_GT
+    #ptsa_GT[1, 0, 1] = ptau3_GT + ptau4_GT
+    #ptsa_GT[2, 0, 2] = ptau4_GT
     
-    ptsas_GT = np.zeros((4, 1, 4, 3))
-    ptsas_GT[0, 0, 1, 0] = ptau2_GT + ptau3_GT + ptau4_GT
-    ptsas_GT[1, 0, 2, 1] = ptau3_GT + ptau4_GT
-    ptsas_GT[2, 0, 3, 2] = ptau4_GT
+    #ptsas_GT = np.zeros((4, 1, 4, 3))
+    #ptsas_GT[0, 0, 1, 0] = ptau2_GT + ptau3_GT + ptau4_GT
+    #ptsas_GT[1, 0, 2, 1] = ptau3_GT + ptau4_GT
+    #ptsas_GT[2, 0, 3, 2] = ptau4_GT
     
     # Compute children and parent dictionaries
     parents, children = compute_parents_children(env.t, env.terminal_state_mask)
@@ -259,7 +388,25 @@ def main():
         gamma=gamma,
         rs=env.state_rewards
     )
+    
+    alpha_log = backward_pass_log(
+        env.p0s,
+        L,
+        env.t,
+        parents,
+        gamma=gamma,
+        rs=env.state_rewards
+    )
+    
     beta = forward_pass(
+        L,
+        env.t,
+        children,
+        gamma=gamma,
+        rs=env.state_rewards
+    )
+    
+    beta_log = forward_pass_log(
         L,
         env.t,
         children,
@@ -273,22 +420,28 @@ def main():
     
     # Drop dummy components
     alpha = alpha[0:-1, 0:4]
+    alpha_log = alpha_log[0:-1, 0:4]
     beta = beta[0:-1, 0:4]
+    beta_log = beta_log[0:-1, 0:4]
     pts = pts[0:-1, 0:4]
-    ptsa = ptsa[0:-1, 0:-1, 0:3]
-    ptsas = ptsas[0:-1, 0:-1, 0:-1, 0:3]
+    #ptsa = ptsa[0:-1, 0:-1, 0:3]
+    #ptsas = ptsas[0:-1, 0:-1, 0:-1, 0:3]
     
     print("Alpha = ")
     print(alpha)
+    np.testing.assert_array_almost_equal(alpha, np.exp(alpha_log))
+    
     print("Beta = ")
     print(beta)
+    np.testing.assert_array_almost_equal(beta, np.exp(beta_log))
+    
     print("Z = {}".format(Z_theta))
+    
     print("p_t(s) = ")
     print(pts)
-    
     np.testing.assert_array_almost_equal(pts, pts_GT)
-    np.testing.assert_array_almost_equal(ptsa, ptsa_GT)
-    np.testing.assert_array_almost_equal(ptsas, ptsas_GT)
+    #np.testing.assert_array_almost_equal(ptsa, ptsa_GT)
+    #np.testing.assert_array_almost_equal(ptsas, ptsas_GT)
     
 
 if __name__ == '__main__':
