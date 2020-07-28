4e125bf705ffcb67198b183b9e52dccbce3dd8c1
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-28 10:55:13 +1000

    Add requirement for python-interface

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='unimodal_irl',
-    version='0.0.1',
+    name="unimodal_irl",
+    version="0.0.1",
     install_requires=[
-        'gym >= 0.2.3',
-        'numpy',
-        'scipy',
-        'matplotlib',
+        "gym >= 0.2.3",
+        "numpy",
+        #'scipy',
+        #'matplotlib',
+        "python-interface",
     ],
     packages=find_packages(),
 )
