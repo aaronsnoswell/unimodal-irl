257cdbd9c82c3b4b55c4003e3b8890a93bc0641f
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-29 17:36:11 +1000

    Add discrete2explicit conversion function
    Converts gym.envs.toy_text.discrete.DiscreteEnv to IExplicitEnv so it can be used with IRL algorithms in this library

--- unimodal_irl/envs/utils.py
+++ unimodal_irl/envs/utils.py
@@ -2,7 +2,10 @@
 
 import copy
 import numpy as np
+import itertools as it
+from types import MethodType
 
+import gym
 from gym import spaces
 
 
@@ -98,5 +101,105 @@ def pad_terminal_mdp(env):
         env._state_action_rewards = state_action_rewards2
     if env.state_action_state_rewards is not None:
         env._state_action_state_rewards = state_action_state_rewards2
+
+    return env
+
+
+def discrete2explicit(EnvClass, env, *, gamma=1.0):
+    """Make a DiscreteEnv compliant with IExplicitEnv
+    
+    Args:
+        EnvClass (class): Class of the environment we are modifying
+        env (gym.envs.toy_text.discrete.DiscreteEnv): Environment to update
+        
+        gamma (float): Discount factor to assign
+    
+    Returns:
+        (gym.Env): A gym environment that is compatible with IExplicitEnv
+    """
+
+    # Work with a copy, not the original
+    env = copy.deepcopy(env)
+
+    env._states = np.arange(env.nS)
+    setattr(EnvClass, "states", property(lambda self: self._states))
+    env._actions = np.arange(env.nA)
+    setattr(EnvClass, "actions", property(lambda self: self._actions))
+    env._p0s = np.array(env.isd)
+    setattr(EnvClass, "p0s", property(lambda self: self._p0s))
+
+    # Build transition dynamics
+    env._t_mat = np.zeros((env.nS, env.nA, env.nS))
+    env._terminal_state_mask = np.zeros_like(env._states)
+    for s1 in env._states:
+        for a in env._actions:
+            for prob, s2, r, done in env.P[s1][a]:
+                env._t_mat[s1, a, s2] = prob
+                if done:
+                    env._terminal_state_mask[s2] = 1.0
+    setattr(EnvClass, "t_mat", property(lambda self: self._t_mat))
+    setattr(
+        EnvClass,
+        "terminal_state_mask",
+        property(lambda self: self._terminal_state_mask),
+    )
+
+    env._parents, env._children = compute_parents_children(
+        env._t_mat, env._terminal_state_mask
+    )
+    setattr(EnvClass, "parents", property(lambda self: self._parents))
+    setattr(EnvClass, "children", property(lambda self: self._children))
+    env._gamma = gamma
+    setattr(EnvClass, "gamma", property(lambda self: self._gamma))
+
+    env._state_rewards = None
+    env._state_action_rewards = None
+    env._state_action_state_rewards = None
+
+    # Infer reward structure from transition tensor
+    _rs = {s: set() for s in env._states}
+    _rsa = {sa: set() for sa in it.product(env._states, env._actions)}
+    _rsas = {sas: set() for sas in it.product(env._states, env._actions, env._states)}
+    for s1 in env._states:
+        for a in env._actions:
+            for prob, s2, r, done in env.P[s1][a]:
+                _rs[s2].add(r)
+                _rsa[(s1, a)].add(r)
+                _rsas[(s1, a, s2)].add(r)
+    _rs = {s: list(rewards) for s, rewards in _rs.items()}
+    _rsa = {sa: list(rewards) for sa, rewards in _rsa.items()}
+    _rsas = {sas: list(rewards) for sas, rewards in _rsas.items()}
+    num_rewards_per_state = [len(rewards) for rewards in _rs.values()]
+    num_rewards_per_state_action = [len(rewards) for rewards in _rsa.values()]
+    num_rewards_per_state_action_state = [len(rewards) for rewards in _rsas.values()]
+
+    if max(num_rewards_per_state) == 1:
+        # This MDP allows for consistent state-only rewards
+        env._state_rewards = np.array([_rs[s][0] for s in env._states])
+    elif max(num_rewards_per_state_action) == 1:
+        # This MDP allows for consistent state-action rewards
+        env._state_action_rewards = np.zeros((env.nS, env.nA))
+        for s1, a in it.product(env._states, env._actions):
+            env._state_action_rewards[s1, a] = _rsa[(s1, a)][0]
+    else:
+        # This MDP requires state-action-state rewards
+        assert (
+            max(num_rewards_per_state_action_state) == 1
+        ), "MDP rewards are stochastic and can't be represented by a linear reward function"
+        env._state_action_state_rewards = np.zeros((env.nS, env.nA, env.nS))
+        for s1, a, s2 in it.product(env._states, env._actions, env._states):
+            env._state_action_rewards[s1, a, s2] = _rsas[(s1, a, s2)][0]
+
+    setattr(EnvClass, "state_rewards", property(lambda self: self._state_rewards))
+    setattr(
+        EnvClass,
+        "state_action_rewards",
+        property(lambda self: self._state_action_rewards),
+    )
+    setattr(
+        EnvClass,
+        "state_action_state_rewards",
+        property(lambda self: self._state_action_state_rewards),
+    )
 
     return env
