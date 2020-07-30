03f62e46d208c9966d112e9090be769c92905192
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-30 16:04:01 +1000

    Move IExplicitEnv getters to convenience helper class

--- unimodal_irl/envs/explicit_env.py
+++ unimodal_irl/envs/explicit_env.py
@@ -7,6 +7,65 @@ are explicit (in the form of a transition matrix, linear weights etc.)
 import interface
 
 
+class ExplicitEnvGetters:
+    """Convenience class that provides getters for protected IExplicitEnv properties"""
+
+    @property
+    def states(self):
+        """Iterable over MDP states"""
+        return self._states
+
+    @property
+    def actions(self):
+        """Iterable over MDP actions"""
+        return self._actions
+
+    @property
+    def p0s(self):
+        """|S| vector of starting state probabilities"""
+        return self._p0s
+
+    @property
+    def t_mat(self):
+        """|S|x|A|x|S| array of transition probabilities"""
+        return self._t_mat
+
+    @property
+    def terminal_state_mask(self):
+        """|S| vector indicating terminal states"""
+        return self._terminal_state_mask
+
+    @property
+    def parents(self):
+        """Dict mapping a state to it's (s, a) parents"""
+        return self._parents
+
+    @property
+    def children(self):
+        """Dict mapping a state to it's (a, s') children"""
+        return self._children
+
+    @property
+    def gamma(self):
+        """Discount factor"""
+        return self._gamma
+
+    @property
+    def state_rewards(self):
+        """|S| vector of linear state reward weights"""
+        return self._state_rewards
+
+    @property
+    def state_action_rewards(self):
+        """|S|x|A| array of linear state-action reward weights"""
+        return self._state_action_rewards
+
+    @property
+    def state_action_state_rewards(self):
+        """|S|x|A|x|S| array of linear state-action-state reward weights"""
+        return self._state_action_state_rewards
+
+
 class IExplicitEnv(interface.Interface):
     """An MDP with explicit dynamics and linear reward"""
 
@@ -58,9 +117,9 @@ class IExplicitEnv(interface.Interface):
     @property
     def state_action_rewards(self):
         """|S|x|A| array of linear state-action reward weights"""
-        return None
+        pass
 
     @property
     def state_action_state_rewards(self):
         """|S|x|A|x|S| array of linear state-action-state reward weights"""
-        return None
+        pass
