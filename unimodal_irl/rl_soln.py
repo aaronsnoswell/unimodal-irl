6ba1f466764f1ef322cadc66c87e59871db6e204
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-30 14:26:08 +1000

    Fix typo with value iteration warning

--- unimodal_irl/rl_soln.py
+++ unimodal_irl/rl_soln.py
@@ -22,7 +22,7 @@ def value_iteration(env, eps=1e-6, verbose=False, max_iter=None):
         (numpy array): |S| vector of state values
     """
 
-    if env.gamma < 1.0:
+    if env.gamma == 1.0:
         warnings.warn(
             "Environment discount factor is 1.0 - value iteration will only converge if all paths terminate in a finite number of steps"
         )
