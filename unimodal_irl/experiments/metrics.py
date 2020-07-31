581e3db5804002a5681e48c725ac77871eb74e4c
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-31 15:24:52 +1000

    Combine ILE and EVD metric calculations to save time

--- unimodal_irl/experiments/metrics.py
+++ unimodal_irl/experiments/metrics.py
@@ -10,24 +10,29 @@ from unimodal_irl.rl_soln import (
 )
 
 
-def ile(env_GT, env_IRL, *, p=1, optimal_policy=None, verbose=False):
-    """Find Inverse Learning Error metric
+def ile_evd(env_GT, env_IRL, *, p=1, optimal_policy=None, verbose=False):
+    """Find Inverse Learning Error and Expected Valud Difference metrics
     
     Inverse Learning Error is defined in "Inverse reinforcement learning in partially
     observable environments." by Choi and Kim, 2011.
+    
+    Expected Value Difference is defined in "Nonlinear inverse reinforcement learning
+    with gaussian processes." by Levine, et al. 2011. EVD is essentially a weighted
+    version of ILE, that only considers states with non-zero starting probability.
     
     Args:
         env_GT (unimodal_irl.envs.explicit.IExplicitEnv) Environment with GT reward
         env_IRL (unimodal_irl.envs.explicit.IExplicitEnv) Environment with learned
             reward
         
-        p (int): p-Norm to use, Choi and Kim and other papers recommend p=1
+        p (int): p-Norm to use for ILE, Choi and Kim and other papers recommend p=1
         optimal_policy (object): Optional shortcut - provide a pre-trained optimal
             policy so we don't have to compute it
         verbose (bool): Extra logging info
     
     Returns:
-        (float): Inverse Learning Error
+        (float): Inverse Learning Error metric
+        (float): Expected Value Difference metric
     """
     # Find the ground truth value of the optimal policy
     if verbose:
@@ -53,51 +58,6 @@ def ile(env_GT, env_IRL, *, p=1, optimal_policy=None, verbose=False):
     if verbose:
         print("Learned policy GT value = \n{}".format(vpi_IRL))
 
-    return np.linalg.norm(vpi_GT - vpi_IRL, ord=p)
-
-
-def evd(env_GT, env_IRL, optimal_policy=None, verbose=False):
-    """Find Expected Value Difference metric
-    
-    Expected Value Difference is defined in "Nonlinear inverse reinforcement learning
-    with gaussian processes." by Levine, et al. 2011.
-    
-    Args:
-        env_GT (unimodal_irl.envs.explicit.IExplicitEnv) Environment with GT reward
-        env_IRL (unimodal_irl.envs.explicit.IExplicitEnv) Environment with learned
-            reward
-        
-        optimal_policy (object): Optional shortcut - provide a pre-trained optimal
-            policy so we don't have to compute it
-        verbose (bool): Extra logging information
-    
-    Returns:
-        (float): Inverse Learning Error
-    """
-    # Find the ground truth value of the optimal policy
-    if verbose:
-        print("Solving for GT value of optimal policy")
-    if optimal_policy is None:
-        v_GT = value_iteration(env_GT, verbose=verbose)
-        q_GT = q_from_v(v_GT, env_GT)
-        pi_GT = EpsilonGreedyPolicy(q_GT)
-    else:
-        pi_GT = optimal_policy
-    vpi_GT = policy_evaluation(env_GT, pi_GT)
-    if verbose:
-        print("Optimal policy GT value = \n{}".format(vpi_GT))
-
-    # Find the value of the optimal policy for the learned reward, when evaluated on
-    # the true reward
-    if verbose:
-        print("Solving for GT value of learned policy")
-    v_IRL = value_iteration(env_IRL, verbose=verbose)
-    q_IRL = q_from_v(v_IRL, env_IRL)
-    pi_IRL = EpsilonGreedyPolicy(q_IRL)
-    vpi_IRL = policy_evaluation(env_GT, pi_IRL)
-    if verbose:
-        print("Learned policy GT value = \n{}".format(vpi_IRL))
-
-    # Expected Value Difference is the expectation under starting distribution of the
-    # difference in GT and learned policy value under the true reward
-    return env_GT.p0s @ (vpi_GT - vpi_IRL)
+    ile = np.linalg.norm(vpi_GT - vpi_IRL, ord=p)
+    evd = env_GT.p0s @ (vpi_GT - vpi_IRL)
+    return ile, evd
