d04deb022ef9bd74c4ef2524292201f4a64edce9
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-07-31 14:58:49 +1000

    Add utility function to find empirical feature expectations from rollouts

--- unimodal_irl/utils.py
+++ unimodal_irl/utils.py
@@ -1,5 +1,6 @@
 """Various utility methods related to IRL algorithms"""
 
+import numpy as np
 import itertools as it
 
 
@@ -54,3 +55,39 @@ def get_rollouts(env, policy, num_rollouts, *, max_episode_length=None, verbose=
             break
 
     return rollouts
+
+
+def empirical_feature_expectations(env, rollouts):
+    """Find empirical discounted feature expectations
+    
+    Args:
+        env (unimodal_irl.envs.explicit.IExplicitEnv): Environment defining dynamics,
+            reward(s) and discount factor
+        rollouts (list): List of [(s, a), (s, a), ..., (s, None)] trajectories
+    
+    Returns:
+        (numpy array): |S| array of state marginals
+        (numpy array): |S|x|A| array of state-action marginals
+        (numpy array): |S|x|A|x|S| array of state-action-state marginals
+    """
+
+    # Find discounted feature expectations
+    phibar_s = np.zeros(env.t_mat.shape[0])
+    phibar_sa = np.zeros(env.t_mat.shape[0 : 1 + 1])
+    phibar_sas = np.zeros(env.t_mat.shape)
+    for r in rollouts:
+
+        if env.state_rewards is not None:
+            for t, (s1, _) in enumerate(r):
+                phibar_s[s1] += (env.gamma ** t) * 1
+
+        if env.state_action_rewards is not None:
+            for t, (s1, a) in enumerate(r[:-1]):
+                phibar_sa[s1, a] += (env.gamma ** t) * 1
+
+        if env.state_action_state_rewards is not None:
+            for t, (s1, a) in enumerate(r[:-1]):
+                s2 = r[t + 1][0]
+                phibar_sas[s1, a, s2] += (env.gamma ** t) * 1
+
+    return phibar_s, phibar_sa, phibar_sas
