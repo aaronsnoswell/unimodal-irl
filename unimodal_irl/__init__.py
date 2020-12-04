fa229d280a0c1046bfd9f652139b415d6ea6b200
Aaron Snoswell <aaronsnoswell@gmail.com>
2020-12-04 12:02:12 +1000

    Expose MaxEnt IRL internal functions at global scope for conveience

--- unimodal_irl/__init__.py
+++ unimodal_irl/__init__.py
@@ -4,5 +4,8 @@ from unimodal_irl.sw_maxent_irl import (
     maxent_log_likelihood,
     maxent_path_logprobs,
     log_partition,
+    nb_backward_pass_log,
+    nb_forward_pass_log,
+    nb_marginals_log,
 )
 from unimodal_irl.zb_maxent_irl import zb_maxent_irl
