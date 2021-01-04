d6576e42502de24da83df169bcb1b065194968cb
Aaron Snoswell <aaronsnoswell@gmail.com>
2021-01-04 19:48:45 +1000

    Expose _deterministic_stateonly methods to module scope

--- unimodal_irl/__init__.py
+++ unimodal_irl/__init__.py
@@ -5,8 +5,11 @@ from unimodal_irl.sw_maxent_irl import (
     maxent_path_logprobs,
     log_partition,
     nb_backward_pass_log,
+    nb_backward_pass_log_deterministic_stateonly,
     nb_forward_pass_log,
+    nb_forward_pass_log_deterministic_stateonly,
     nb_marginals_log,
+    nb_marginals_log_deterministic_stateonly
 )
 from unimodal_irl.zb_maxent_irl import zb_maxent_irl
 from unimodal_irl.bv_maxlikelihood_irl import bv_maxlikelihood_irl
