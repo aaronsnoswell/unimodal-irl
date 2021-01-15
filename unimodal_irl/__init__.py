b59326d9ace6bb0af6c9ebcfc8c2ad638b7305f0
Aaron Snoswell <aaronsnoswell@gmail.com>
2021-01-15 12:44:38 +1000

    All imports at root level

--- unimodal_irl/__init__.py
+++ unimodal_irl/__init__.py
@@ -1,15 +1,7 @@
 # Move imports to root namespace for convenience
-from unimodal_irl.sw_maxent_irl import (
-    sw_maxent_irl,
-    maxent_log_likelihood,
-    maxent_path_logprobs,
-    log_partition,
-    nb_backward_pass_log,
-    nb_backward_pass_log_deterministic_stateonly,
-    nb_forward_pass_log,
-    nb_forward_pass_log_deterministic_stateonly,
-    nb_marginals_log,
-    nb_marginals_log_deterministic_stateonly
-)
-from unimodal_irl.zb_maxent_irl import zb_maxent_irl
-from unimodal_irl.bv_maxlikelihood_irl import bv_maxlikelihood_irl
+from unimodal_irl.sw_maxent_irl import *
+from unimodal_irl.zb_maxent_irl import *
+from unimodal_irl.bv_maxlikelihood_irl import *
+from unimodal_irl.utils import *
+from unimodal_irl.metrics import *
+from unimodal_irl.vgd import *
