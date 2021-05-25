a6ebb4114c1611f8de7c18af83c2fc865a984944
Aaron Snoswell <aaronsnoswell@fastmail.com>
2021-05-25 18:29:53 +1000

    Add incremental mean estimator utility function

--- unimodal_irl/utils.py
+++ unimodal_irl/utils.py
@@ -1,3 +1,19 @@
 """Various utility methods related to IRL algorithms"""
 
-# Nothing here yet...
+
+def incremental_mean(x, prev_mean, k):
+    """Compute a mean incrementally over a dataset of num items
+
+    This is useful when we might run out of memory storing all the individual components of the mean
+
+    Args:
+        x (any): The current (k-th) datapoint
+        prev_mean (any): The previous datapoint. If set to None, then we assert k == 1
+        k (int): The index of the current datapoint, starting from 1
+    """
+    if prev_mean is None:
+        assert k == 1
+        return x
+    else:
+        diff = x - prev_mean
+        return prev_mean + diff / k
