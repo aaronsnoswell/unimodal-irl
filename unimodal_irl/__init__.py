ad9a74496854f6db193ec9f788ecb03368e777f3
Aaron Snoswell <aaronsnoswell@fastmail.com>
2021-05-26 18:29:02 +1000

    Expose GIRL fns at module level

--- unimodal_irl/__init__.py
+++ unimodal_irl/__init__.py
@@ -3,6 +3,7 @@ from unimodal_irl.sw_maxent_irl import *
 from unimodal_irl.sw_modelfree_maxent_irl import *
 from unimodal_irl.zb_maxent_irl import *
 from unimodal_irl.bv_maxlikelihood_irl import *
+from unimodal_irl.ra_gradient_irl import *
 from unimodal_irl.utils import *
 from unimodal_irl.metrics import *
 from unimodal_irl.vgd import *
